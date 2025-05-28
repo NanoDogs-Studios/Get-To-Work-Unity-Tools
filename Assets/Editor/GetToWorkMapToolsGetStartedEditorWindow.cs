2f47fd2659dd132e4def1dc6241d9da8d36427fe
Dylan Day <Dylan101day@gmail.com>
2025-05-28 01:44:25 +0100

    dang i havent pushed in like a month oops
    upgraded to unity 6, improved systems other stuff

--- Assets/Editor/GetToWorkMapToolsGetStartedEditorWindow.cs
+++ Assets/Editor/GetToWorkMapToolsGetStartedEditorWindow.cs
@@ -1,159 +1,162 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
-using UnityEngine.SceneManagement;
 using UnityEditor.SceneManagement;
-using UnityEditor.PackageManager.UI;
 using UnityEditor.PackageManager.Requests;
 using UnityEditor.PackageManager;
-using System.Threading.Tasks;
-using System.IO;
-using System.Data;
 
-public class GetToWorkMapToolsGetStartedEditorWinow : EditorWindow
+namespace Nanodogs.GetToWork.MapTools
 {
-    private string mapName = "Map Name";
-    private string mapAuthor = "Map Author";
-
-    private Texture2D bannerImage;
-    bool showWindow = false;
-
-    static TextAsset dataasset;
-
-    [MenuItem("Get To Work Tools/Map Tools/Get Started")]
-    public static void ShowWindow()
+    /// <summary>
+    /// Editor window for getting started with Get To Work Map Tools.
+    /// This window provides options to read documentation, install ProBuilder, load a template scene,
+    /// </summary>
+    public class GetToWorkMapToolsGetStartedEditorWinow : EditorWindow
     {
-        const string k_ProjectOpened = "ProjectOpened";
+        private string mapName = "Map Name";
+        private string mapAuthor = "Map Author";
 
+        private Texture2D bannerImage;
+        bool showWindow = false;
 
-        GetToWorkMapToolsGetStartedEditorWinow window = GetWindow<GetToWorkMapToolsGetStartedEditorWinow>();
-        window.titleContent = new GUIContent("Getting Started (G2W MTools)");
-        window.Show();
-    }
+        static TextAsset dataasset;
 
-    private void OnEnable()
-    {
-        // Load the banner image (make sure it's in a Resources folder)
-        bannerImage = Resources.Load<Texture2D>("MapTools/gtwtoolsbanner");
-    }
-
-    private void OnGUI()
-    {
-
-        #region styles
-        GUIStyle centered = new GUIStyle(GUI.skin.label)
+        [MenuItem("Get To Work Tools/Map Tools/Get Started")]
+        public static void ShowWindow()
         {
-            alignment = TextAnchor.MiddleCenter,
-        };
+            GetToWorkMapToolsGetStartedEditorWinow window = GetWindow<GetToWorkMapToolsGetStartedEditorWinow>();
+            window.titleContent = new GUIContent("Getting Started (G2W MTools)");
+            window.Show();
+        }
 
-        GUIStyle centeredButtonBig = new GUIStyle(GUI.skin.button)
+        private void OnEnable()
         {
-            alignment = TextAnchor.MiddleCenter,
-            hover = { textColor = Color.cyan },
-            fontSize = 15,
-            fixedHeight = 50
-        };
+            // Load the banner image (make sure it's in a Resources folder)
+            bannerImage = Resources.Load<Texture2D>("MapTools/gtwtoolsbanner");
+        }
 
-        GUIStyle centeredBigger = new GUIStyle(GUI.skin.label)
+        private void OnGUI()
         {
-            alignment = TextAnchor.MiddleCenter,
-            fontSize = 16
-        };
-        #endregion
 
-        if (bannerImage != null)
-        {
-            Rect bannerRect = new Rect(0, 0, position.width, 100);
-            GUI.DrawTexture(bannerRect, bannerImage, ScaleMode.ScaleAndCrop);
-            GUIStyle textStyle = new GUIStyle(GUI.skin.label)
+            #region styles
+            GUIStyle centered = new GUIStyle(GUI.skin.label)
             {
                 alignment = TextAnchor.MiddleCenter,
-                fontSize = 22,
-                normal = { textColor = Color.black },
-                hover = { textColor = Color.black }
             };
-            GUI.Label(bannerRect, "Getting Started With G2W Map Tools", textStyle);
-            GUILayout.Space(110);
+
+            GUIStyle centeredButtonBig = new GUIStyle(GUI.skin.button)
+            {
+                alignment = TextAnchor.MiddleCenter,
+                hover = { textColor = Color.cyan },
+                fontSize = 15,
+                fixedHeight = 50
+            };
+
+            GUIStyle centeredBigger = new GUIStyle(GUI.skin.label)
+            {
+                alignment = TextAnchor.MiddleCenter,
+                fontSize = 16
+            };
+            #endregion
+
+            if (bannerImage != null)
+            {
+                Rect bannerRect = new Rect(0, 0, position.width, 100);
+                GUI.DrawTexture(bannerRect, bannerImage, ScaleMode.ScaleAndCrop);
+                GUIStyle textStyle = new GUIStyle(GUI.skin.label)
+                {
+                    alignment = TextAnchor.MiddleCenter,
+                    fontSize = 22,
+                    normal = { textColor = Color.black },
+                    hover = { textColor = Color.black }
+                };
+                GUI.Label(bannerRect, "Getting Started With G2W Map Tools", textStyle);
+                GUILayout.Space(110);
+            }
+            else
+            {
+                GUILayout.Label("(Banner image not found. Place 'banner.png' in Resources.)", EditorStyles.centeredGreyMiniLabel);
+            }
+
+            GUILayout.Label("Getting Started!", centered);
+
+            EditorGUILayout.Space(10);
+
+            EditorGUILayout.HelpBox("Welcome to the Get To Work Map Tools! Credits: LaymGlitched - Creating Map Tools & G2WMP, Isto Inc. - For Creating G2W, Yyk - Helping with G2WMP", MessageType.Info);
+
+            EditorGUILayout.Space(10);
+
+            if (GUILayout.Button("Read the Docs", centeredButtonBig, GUILayout.Height(50), GUILayout.ExpandWidth(true)))
+            {
+                Application.OpenURL("https://laymglitched.gitbook.io/get-to-work-modding");
+            }
+
+            if (GUILayout.Button("Install ProBuilder", centeredButtonBig, GUILayout.Height(50), GUILayout.ExpandWidth(true)))
+            {
+                ProbuilderSetup();
+            }
+
+            if (GUILayout.Button("Load the Template Scene", centeredButtonBig, GUILayout.Height(50), GUILayout.ExpandWidth(true)))
+            {
+                EditorSceneManager.OpenScene("Assets/G2WTools/MapTools/TemplateScene.unity");
+            }
+
+            if (GUILayout.Button("Create Map Data", centeredButtonBig, GUILayout.Height(50), GUILayout.ExpandWidth(true)))
+            {
+                showWindow = true;
+            }
+            if (showWindow)
+            {
+                BeginWindows();
+                GUILayout.Window(0, new Rect(0, 0, 200, 200), DoWindow, "Map Data");
+                EndWindows();
+            }
         }
-        else
+
+        void DoWindow(int unusedWindowID)
         {
-            GUILayout.Label("(Banner image not found. Place 'banner.png' in Resources.)", EditorStyles.centeredGreyMiniLabel);
+            mapName = EditorGUILayout.TextField("Map Name", mapName);
+            mapAuthor = EditorGUILayout.TextField("Map Author", mapAuthor);
+            this.Repaint();
+
+            // Update metadata
+            MapData.Instance.SetData(mapName, mapAuthor);
+
+            if (GUILayout.Button("Create Map Data"))
+            {
+                dataasset = new TextAsset($"\nMap Name: {mapName}\nMap Author: {mapAuthor}");
+                AssetDatabase.CreateAsset(dataasset, $"Assets/AssetBundles/{mapName}.txt");
+                Debug.Log(AssetDatabase.GetAssetPath(dataasset));
+                AssetDatabase.Refresh();
+            }
+
+            if (GUILayout.Button("Close"))
+            {
+                showWindow = false; // Close the window
+            }
+
+            GUI.DragWindow();
         }
 
-        GUILayout.Label("Getting Started!", centered);
 
-        EditorGUILayout.Space(10);
+        static AddRequest Request;
 
-        EditorGUILayout.HelpBox("Welcome to the Get To Work Map Tools! Credits: LaymGlitched - Creating Map Tools & G2WMP, Isto Inc. - For Creating G2W, Yyk - Helping with G2WMP", MessageType.Info);
-
-        EditorGUILayout.Space(10);
-        if(GUILayout.Button("Install ProBuilder", centeredButtonBig, GUILayout.Height(50), GUILayout.ExpandWidth(true)))
+        static void ProbuilderSetup()
         {
-            ProbuilderSetup();
+            Request = Client.Add("com.unity.probuilder");
+            EditorApplication.update += Progress;
         }
 
-        if (GUILayout.Button("Load the Template Scene", centeredButtonBig, GUILayout.Height(50), GUILayout.ExpandWidth(true)))
+        static void Progress()
         {
-            EditorSceneManager.OpenScene("Assets/G2WTools/MapTools/TemplateScene.unity");
-        }
+            if (Request.IsCompleted)
+            {
+                if (Request.Status == StatusCode.Success)
+                    Debug.Log("Installed: " + Request.Result.packageId);
+                else if (Request.Status >= StatusCode.Failure)
+                    Debug.Log(Request.Error.message);
 
-        if (GUILayout.Button("Create Map Data", centeredButtonBig, GUILayout.Height(50), GUILayout.ExpandWidth(true)))
-        {
-            showWindow = true;
-        }
-        if (showWindow)
-        {
-            BeginWindows();
-            GUILayout.Window(0, new Rect(0, 0, 200, 200), DoWindow, "Map Data");
-            EndWindows();
-        }
-    }
-
-    void DoWindow(int unusedWindowID)
-    {
-        mapName = EditorGUILayout.TextField("Map Name", mapName);
-        mapAuthor = EditorGUILayout.TextField("Map Author", mapAuthor);
-        this.Repaint();
-
-        // Update metadata
-        MapData.Instance.SetData(mapName, mapAuthor);
-
-        if (GUILayout.Button("Create Map Data"))
-        {
-            dataasset = new TextAsset($"\nMap Name: {mapName}\nMap Author: {mapAuthor}");
-            AssetDatabase.CreateAsset(dataasset, $"Assets/AssetBundles/{mapName}.txt");
-            Debug.Log(AssetDatabase.GetAssetPath(dataasset));
-            AssetDatabase.Refresh();
-        }
-
-        if (GUILayout.Button("Close"))
-        {
-            showWindow = false; // Close the window
-        }
-
-        GUI.DragWindow();
-    }
-
-
-    static AddRequest Request;
-
-    static void ProbuilderSetup()
-    {
-        Request = Client.Add("com.unity.probuilder");
-        EditorApplication.update += Progress;
-    }
-
-    static void Progress()
-    {
-        if (Request.IsCompleted)
-        {
-            if (Request.Status == StatusCode.Success)
-                Debug.Log("Installed: " + Request.Result.packageId);
-            else if (Request.Status >= StatusCode.Failure)
-                Debug.Log(Request.Error.message);
-
-            EditorApplication.update -= Progress;
+                EditorApplication.update -= Progress;
+            }
         }
     }
 }
